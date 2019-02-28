062b77af0d58d44a01ea640eb786d1780f3c23ba
Shefali Lachhar <shefali.lachhar@gmail.com>
2019-02-28 09:58:04 +0530

    Added inventory repository and insert server commands for inventory

---
+++ SMS_Api/Application/Inventory/InventoryServerCommands.Insert.cs
@@ -0,0 +1,42 @@
+﻿using SMS_Api.Common.Constants;
+using SMS_Api.Context;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace SMS_Api.Application.Inventory
+{
+    public static partial class InventoryServerCommands
+    {
+        public static bool CreateInventory(SMS_Library.Business.Models.Inventory inventory)
+        {
+            try
+            {
+                StationaryManagementSystem.SetSQLCommand(DBConstants.InventoryCreate);
+
+                StationaryManagementSystem.smsCmd.CommandType = CommandType.StoredProcedure;
+                StationaryManagementSystem.smsCmd.Parameters.Add("@InventoryTypeId", SqlDbType.Int).Value = inventory.InventoryTypeId;
+                StationaryManagementSystem.smsCmd.Parameters.Add("@InventoryName", SqlDbType.NVarChar).Value = inventory.InventoryName;
+                StationaryManagementSystem.smsCmd.Parameters.Add("@UnitPrice", SqlDbType.Int).Value = inventory.UnitPrice;
+                StationaryManagementSystem.smsCmd.Parameters.Add("@Quantity", SqlDbType.Int).Value = inventory.Quantity;
+                StationaryManagementSystem.smsCmd.Parameters.Add("@MinRequiredQuantity", SqlDbType.Int).Value = inventory.MinRequiredQuantity;
+                StationaryManagementSystem.smsCmd.Parameters.Add("@MaxRequiredQuantity", SqlDbType.Int).Value = inventory.MaxRequiredQuantity;
+                StationaryManagementSystem.smsCmd.Parameters.Add("@UpdatedBy", SqlDbType.Int).Value = inventory.UpdatedBy;
+                StationaryManagementSystem.smsCmd.Parameters.Add("@CreatedBy", SqlDbType.Int).Value = inventory.CreatedBy;
+                StationaryManagementSystem.smsConn.Open();
+                object o = StationaryManagementSystem.smsCmd.ExecuteScalar();
+                if (o != null)
+                {
+                    string id = o.ToString();
+                }
+            }
+            finally
+            {
+                StationaryManagementSystem.smsConn.Close();
+            }
+            return true;
+        }
+    }
+}
--- SMS_Library/Business/Models/Inventory.cs
+++ SMS_Library/Business/Models/Inventory.cs
@@ -6,24 +6,24 @@ using System.Threading.Tasks;
 
 namespace SMS_Library.Business.Models
 {
-    public class Inventary
+    public class Inventory
     {
-        public int InventaryId { get; set; }
-        public int InventaryTypeId { get; set; }
-        public string InventaryName { get; set; }
+        public int InventoryId { get; set; }
+        public int InventoryTypeId { get; set; }
+        public string InventoryName { get; set; }
         public decimal UnitPrice { get; set; }
         public int Quantity { get; set; }
         public int MinRequiredQuantity { get; set; }
         public int MaxRequiredQuantity { get; set; }
         public int CreatedBy { get; set; }
         public DateTime CreatedOn { get; set; }
-        public int UndatedBy { get; set; }
+        public int UpdatedBy { get; set; }
         public DateTime UpdatedOn { get; set; }
     }
 
-    public class InventaryType
+    public class InventoryType
     {
-        public int InventaryTypeId { get; set; }
-        public string InventaryTypeName { get; set; }
+        public int InventoryTypeId { get; set; }
+        public string InventoryTypeName { get; set; }
     }
 }
